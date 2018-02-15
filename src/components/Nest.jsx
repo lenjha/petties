007d56b085f83311f7a83b6765a419bcd32af9a0
Lena Ha and Mikey Pinaud <lena@>
2018-02-15 13:57:50 -0800

    add death state

--- src/components/Nest.jsx
+++ src/components/Nest.jsx
@@ -25,43 +25,64 @@ class Nest extends React.Component {
   componentDidMount() {
     this.hungerIntervals = setInterval(() =>
       this.updateHunger(),
-    1000
+    50
     );
     this.happinessIntervals = setInterval(() =>
       this.updateHappiness(),
-    1000
+    50
     );
     this.energyIntervals = setInterval(() =>
       this.updateEnergy(),
-    1000
+    100
     );
-    this.healthIntervals = setInterval(() =>
-      this.updateHealth(),
-    1000
-    );
+    // this.healthIntervals = setInterval(() =>
+    //   this.updateHealth(),
+    // 1000
+    // );
   }
+
 
   updateHunger() {
     let newSatiety = this.state.satiety;
-    newSatiety -= 1;
+    if (this.state.satiety > 0) {
+      newSatiety -= 1;
+    } else {
+      this.updateHealth();
+    }
     this.setState({satiety : newSatiety});
   }
   updateHappiness() {
     let newHappiness = this.state.happiness;
-    newHappiness -= 1;
+    if (this.state.happiness > 0) {
+      newHappiness -= 1;
+    } else {
+      this.updateHealth();
+    }
     this.setState({happiness : newHappiness});
   }
   updateEnergy() {
     let newEnergy = this.state.energy;
-    newEnergy -= 1;
+    if (this.state.energy > 0) {
+      newEnergy -= 1;
+    } else {
+      this.updateHealth();
+    }
     this.setState({energy : newEnergy});
   }
+
 
   updateHealth() {
     let newHealth = this.state.health;
     newHealth = this.state.satiety + this.state.happiness + this.state.energy;
     this.setState({health : newHealth});
+    if (this.state.health === 0) {
+      clearInterval(this.hungerIntervals);
+      clearInterval(this.happinessIntervals);
+      clearInterval(this.energyIntervals);
+      console.log('DED');
+    }
   }
+
 
 
   render(){
